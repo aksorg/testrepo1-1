d44f9e3fb9f1fcc4294f1e295b3269c5bfb6fdd2
Unnipillai Akshai <92842244+akshaiu@users.noreply.github.com>
2021-11-11 21:22:00 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,45 +1,57 @@
 provider "azurerm" {
   features {}
+} 
+
+locals {
+  fqdns   =[]
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  count    = var.create_resource_group ? 1 : 0
+  name     = var.acrResourceGroup
+  location = var.acrLocation
 }
 
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_key_vault" "keyvault" {
-  name                        = var.keyvault_name
-  location                    = var.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  enabled_for_disk_encryption = var.enabled_for_disk_encryption
-  enabled_for_deployment      = var.enabled_for_deployment
-  enabled_for_template_deployment= var.enabled_for_template_deployment
-  enable_rbac_authorization   = var.enable_rbac_authorization
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_enabled         = true
-  soft_delete_retention_days  = var.soft_delete_retention_days
-  purge_protection_enabled    = var.purge_protection_enabled
-
-  sku_name = var.sku_name
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = var.key_permissions
-
-    secret_permissions = var.key_permissions
-
-    storage_permissions = var.storage_permissions
+resource "azurerm_container_registry" "acr" {
+  name                = var.acrName
+  resource_group_name = var.acrResourceGroup
+  location            = var.acrLocation
+  sku                 = "Premium"
+  admin_enabled       = false
+  dynamic "georeplications" {
+  for_each = var.georeplications
+  content {
+    location = georeplications.value.location
+    zone_redundancy_enabled = georeplications.value.zone_redundancy_enabled
   }
-
-   network_acls {
-     default_action = var.default_action 
-     bypass         = var.bypass
-     virtual_network_subnet_ids = var.virtual_network_subnet_ids
-   }
 }
 
 
+
+
+  network_rule_set {
+    default_action = "Deny" 
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_private_endpoint" "" {
+  name                = "example-endpoint"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = data.azurerm_subnet.subnet.id
+  private_service_connection {
+      name                              = "example-privateserviceconnection"
+      is_manual_connection              = true
+      private_connection_resource_id    = merge("/subscriptions/",var.acrSubscription,"/resourceGroups/",var.acrResourceGroup,"/providers/Microsoft.ContainerRegistry/registries/",var.acrName)
+      subresource_names                 = ["registry"]
+  }
+  dynamic "custom_dns_configs" {
+    for_each = local.fqdns
+    content{
+      fqdn = local.fqdns
+    }
+  }
+}
